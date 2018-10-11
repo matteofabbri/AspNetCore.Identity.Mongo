46e637b59cf4acd53715504b4a72cf950fd51290
Matteo Fabbri <matteo@phascode.org>
2018-10-11 16:48:31 +0200

    Testing environment

---
+++ SelfCheck/UserManagerTester.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Identity;
+using SelfCheck.Controllers;
+
+namespace SelfCheck
+{
+    public class UserManagerTester
+    {
+
+
+    }
+}
