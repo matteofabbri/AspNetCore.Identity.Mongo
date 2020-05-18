7ed6a949f256b77541486c80ca75ae2d21dcc6ae
Matteo Fabbri <k2dkca@splm.siemens.com>
2020-05-18 08:07:56 +0200

    Fixed #54 duplicated MongoUtils

--- AspNetCore.Identity.Mongo/MongoIdentityExtensions.cs
+++ AspNetCore.Identity.Mongo/MongoIdentityExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using AspNetCore.Identity.Mongo.Model;
+using AspNetCore.Identity.Mongo.Mongo;
 using AspNetCore.Identity.Mongo.Stores;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
-using MongoDB.Driver;
 
 namespace AspNetCore.Identity.Mongo
 {
@@ -44,7 +44,7 @@ namespace AspNetCore.Identity.Mongo
 	        where TRole : MongoRole
 	    {
             var dbOptions = new MongoIdentityOptions();
-	        setupDatabaseAction(dbOptions);
+			setupDatabaseAction(dbOptions);
 
 	        var builder = services.AddIdentity<TUser, TRole>(setupIdentityAction ?? (x => { }));
 	        
@@ -58,8 +58,8 @@ namespace AspNetCore.Identity.Mongo
 	        var userCollection =  MongoUtil.FromConnectionString<TUser>(dbOptions.ConnectionString, dbOptions.UsersCollection);
 	        var roleCollection = MongoUtil.FromConnectionString<TRole>(dbOptions.ConnectionString, dbOptions.RolesCollection);
 
-	        services.AddSingleton<IMongoCollection<TUser>>(x => userCollection);
-	        services.AddSingleton<IMongoCollection<TRole>>(x => roleCollection);
+	        services.AddSingleton(x => userCollection);
+	        services.AddSingleton(x => roleCollection);
 
 	        // Identity Services
             services.AddTransient<IRoleStore<TRole>>(x => new RoleStore<TRole>(roleCollection));
