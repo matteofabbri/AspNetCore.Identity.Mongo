887f1dc80abe16e9f35c417e824b1aa261307536
Matteo Fabbri <k2dkca@splm.siemens.com>
2020-07-26 10:34:08 +0200

    migrator

--- DbMigrator/V1/MongoRoleV1.cs
+++ DbMigrator/V1/MongoRoleV1.cs
@@ -1,6 +1,4 @@
-// Warning: Some assembly references could not be resolved automatically. This might lead to incorrect decompilation of some parts,
-// for ex. property getter/setter access. To get optimal decompilation results, please manually add the missing references to the list of loaded assemblies.
-// MongoRole
+
 using Microsoft.AspNetCore.Identity;
 
 namespace DbMigrator.V1
