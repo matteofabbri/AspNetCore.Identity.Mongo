11982179b41b8913d4835f68552e009168a19e0a
vova3211 <buriyvo@gmail.com>
2021-06-22 13:36:48 +0300

    added AddMongoDbStores extension to register only stores.

---
+++ src/AspNetCore.Identity.Mongo/Mongo/TypeConverterResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AspNetCore.Identity.Mongo.Mongo
+{
+    internal static class TypeConverterResolver
+    {
+        internal static void RegisterTypeConverter<T, TC>() where TC : TypeConverter
+        {
+            Attribute[] attr = new Attribute[1];
+            TypeConverterAttribute vConv = new TypeConverterAttribute(typeof(TC));
+            attr[0] = vConv;
+            TypeDescriptor.AddAttributes(typeof(T), attr);
+        }
+    }
+}
