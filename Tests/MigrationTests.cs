0bf17b4f07f554d4181b17ab7c224d38d203a049
Jonas <jonas@gottschau.dk>
2021-11-04 21:38:26 +0100

    fix migration 5->6 not run
    fixes if DatabaseVersion == 4, migration 5 -> 6 will not run resulting in invalid schema.
    
    adds build.yml github action which builds and runs test on PR's and push'es to master branch.
    
    also addresses  #81

---
+++ Tests/MigrationTests.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Identity.Mongo.Migrations;
+using AspNetCore.Identity.Mongo.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MigrationTests
+    {
+        private IDisposable _runner;
+        private IMongoClient _client;
+        private IMongoDatabase _db;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var runner = Mongo2Go.MongoDbRunner.Start();
+            _client = new MongoClient(runner.ConnectionString);
+            _db = _client.GetDatabase("migration-tests");
+            _runner = runner;
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _runner.Dispose();
+        }
+
+        [Test, Category("unit")]
+        public void Apply_Schema4_AllMigrationsApplied()
+        {
+            // ARRANGE
+            var history = _db.GetCollection<MigrationHistory>("migrations");
+            var users = _db.GetCollection<MigrationMongoUser>("users");
+            var roles = _db.GetCollection<MongoRole<ObjectId>>("roles");
+            var initialVersion = 4;
+            var existingHistory = new List<MigrationHistory>
+            {
+                new MigrationHistory
+                {
+                    Id = ObjectId.GenerateNewId(),
+                    DatabaseVersion = 3,
+                    InstalledOn = DateTime.UtcNow.AddDays(-2)
+                },
+                new MigrationHistory
+                {
+                    Id = ObjectId.GenerateNewId(),
+                    DatabaseVersion = initialVersion,
+                    InstalledOn = DateTime.UtcNow.AddDays(-1)
+                }
+            };
+            history.InsertMany(existingHistory);
+
+
+            // ACT
+            Migrator.Apply<MigrationMongoUser, MongoRole<ObjectId>, ObjectId>(history, users, roles);
+
+            // ASSERT
+            var historyAfter = history
+                .Find("{}")
+                .SortBy(h => h.DatabaseVersion)
+                .ToList();
+
+            var expectedHistoryObjectsAfter = Migrator.CurrentVersion - initialVersion + existingHistory.Count;
+            Assert.That(historyAfter.Count, Is.EqualTo(expectedHistoryObjectsAfter),
+                () => "Expected all migrations to run");
+            Assert.That(historyAfter.Last().DatabaseVersion, Is.EqualTo(Migrator.CurrentVersion));
+        }
+    }
+}
